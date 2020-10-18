b2cb490d955894d0dd6f4e268761775e114f42f0
Alcenir Costa <alcenir.1994@gmail.com>
2020-10-18 00:23:33 -0300

    Using extends in classes

--- index.php
+++ index.php
@@ -1,51 +1,49 @@
 <?php
+// Moto e Carro são extensoes de Veiculo, por isso, herdão os mesmos parametros e funcoes da classe Mãe (Veículo)
+class Vechicle{
+  public $model;
+  public $color;
+  public $year;
+  public $vechicle;
 
-class Login {
-
-    private $email;
-    private $password;
-    private $name;
-
-    public function __construct($email,$password,$name){
-      $this->setName($name);
-      $this->setEmail($email);
-      $this->setPassword($password);
-    }
-
-    public function getEmail(){
-        return $this->email;
-    }
-    public function setEmail($email){
-        $cleanEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
-        $this->email = $cleanEmail;
-    }
-    public function getPassword(){
-        return $this->password;
-    }
-    public function setPassword($password){
-        $this->password = $password;
-    }
-    public function getName(){
-      return $this->name;
-  }
-  public function setName($name){
-      $this->name = $name;
+  public function move(){
+    echo $this->vechicle." is moving <br/>";
   }
 
-    public function verify(){
-      if($this->email == "teste@teste.com" && $this->password == "1234"):
-        echo"access granted";
-      else:
-        echo "access denied";
-      endif;
-    }
+  public function stop(){
+    echo $this->vechicle." stoped <br/>";
+  }
+
 }
 
-$logar = new Login("teste@teste.com","1234","Alcenir");
-$logar->verify();
-echo "<br/>";
-echo $logar->getEmail();
-echo "<hr>";
-var_dump($logar);
+class Car extends Vechicle{
+  public function info(){
+   echo 'Carro--> Modelo: '.$this->model.'; <br/> Ano:'.$this->year.';<br/> Cor:'.$this->color; 
+  }
+}
 
+class Motorcicle extends Vechicle{
+  public function info(){
+    echo 'Moto--> Modelo: '.$this->model.'; <br/> Ano:'.$this->year.';<br/> Cor:'.$this->color; 
+   }
+}
+$carrinho = new Car();
+$carrinho->color = "Blue";
+$carrinho->year = 2018;
+$carrinho->model = 'Fiat';
+$carrinho->vechicle = 'Carro';
+$carrinho->move();
+$carrinho->stop();
+
+
+$motocicleta = new Motorcicle();
+$motocicleta->model = 'Kawasaki';
+$motocicleta->color = "green";
+$motocicleta->year = 2020;
+$motocicleta->vechicle= 'Moto';
+$motocicleta->move();
+$motocicleta->stop();
+
+print_r($carrinho);
+print_r($motocicleta);
 ?>
