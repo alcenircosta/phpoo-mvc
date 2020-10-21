1786f553cfae1e138122ea26f921e6d23489f06d
Alcenir Costa <alcenir.1994@gmail.com>
2020-10-21 14:32:23 -0300

    Created Abstract classes and methods

--- index.php
+++ index.php
@@ -1,58 +1,46 @@
 <?php
 // Moto e Carro são extensoes de Veiculo, por isso, herdão os mesmos parametros e funcoes da classe Mãe (Veículo)
-class Vechicle{
-  private $model;
-  public $color;
-  public $year;
-  public $vechicle;
+abstract class Bank {
+  protected $balance;
+  protected $balance_limit;
+  protected $interest;
 
-
-  public function setModel($model){
-    $this->model = $model;
+  public function setSaldo($saldo){
+    $this->saldo = $saldo;
   }
-  public function getModel(){
-    return $this->model;
+  public function getSaldo(){
+    return $this->saldo;
   }
-  private function move(){
-    echo $this->vechicle." is moving <br/>";
-  }
-  // Só tenho acesso externo à classe se houver um método publico que acesse ela e retorne seu valor/tarefa.
-  // Esse metodo publico acessa o privado move
-  public function ismoving(){
-    if($this->move() != "Stoped"):
-      echo "MOVING!!!";
-    else:
-      echo 'Stoped :(';
-    endif;
-  }
-
-  public function stop(){
-    echo $this->vechicle." stoped <br/>";
-  }
+  abstract protected function Sacar($valor);
+  abstract protected function Depositar($valor);
 
 }
 
-class Car extends Vechicle{
-  public function info(){
-   echo 'Carro--> Modelo: '.$this->model.'; <br/> Ano:'.$this->year.';<br/> Cor:'.$this->color; 
+class Itau extends Bank{
+  public function Sacar($valor){
+    $this->saldo -= $valor;
+    echo "Sacou: ".$valor;
   }
-  public function setModel($model){
-    $this->model = $model;
-  }
-  public function getModel(){
-    return $this->model;
+  public function Depositar($valor){
+    $this->saldo += $valor;
+    echo "Depositou: ".$valor;
   }
 }
 
-class Motorcicle extends Vechicle{
-  public function info(){
-    echo 'Moto--> Modelo: '.$this->model.'; <br/> Ano:'.$this->year.';<br/> Cor:'.$this->color; 
-   }
-}
+$itau = new Itau();
+$itau->setSaldo(5000);
+echo "Saldo: -> ";
+echo $itau->getSaldo();
+echo "<hr>";
+$itau->sacar(1000);
+echo "Saldo: -> ";
+echo $itau->getSaldo();
+echo "<hr>";
 
-$truck = new Vechicle();
-$truck->setModel("Volkswagem");
-echo $truck->getModel();
-$truck->ismoving();
+echo "<br/>";
+$itau->Depositar(2000);
+echo "Saldo: -> ";
+echo $itau->getSaldo();
+echo "<hr>";
 
 ?> 
