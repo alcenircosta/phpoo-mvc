a036aa797b8d5bf08f82cc6248c0c25f37af8f1e
Alcenir Costa <alcenir.1994@gmail.com>
2020-10-21 14:58:40 -0300

    Const, self & parent

--- index.php
+++ index.php
@@ -1,46 +1,21 @@
 <?php
-// Moto e Carro são extensoes de Veiculo, por isso, herdão os mesmos parametros e funcoes da classe Mãe (Veículo)
-abstract class Bank {
-  protected $balance;
-  protected $balance_limit;
-  protected $interest;
+ class Pessoa {
+   const nome = "Alcenir ";
 
-  public function setSaldo($saldo){
-    $this->saldo = $saldo;
-  }
-  public function getSaldo(){
-    return $this->saldo;
-  }
-  abstract protected function Sacar($valor);
-  abstract protected function Depositar($valor);
+   public function showName(){
+     echo self::nome;
+   }
+ }
 
-}
+class Alcenir extends Pessoa {
+  const nome = "Costa";
 
-class Itau extends Bank{
-  public function Sacar($valor){
-    $this->saldo -= $valor;
-    echo "Sacou: ".$valor;
-  }
-  public function Depositar($valor){
-    $this->saldo += $valor;
-    echo "Depositou: ".$valor;
+  public function showName(){
+    echo parent::nome;
+    echo self::nome;
   }
 }
 
-$itau = new Itau();
-$itau->setSaldo(5000);
-echo "Saldo: -> ";
-echo $itau->getSaldo();
-echo "<hr>";
-$itau->sacar(1000);
-echo "Saldo: -> ";
-echo $itau->getSaldo();
-echo "<hr>";
-
-echo "<br/>";
-$itau->Depositar(2000);
-echo "Saldo: -> ";
-echo $itau->getSaldo();
-echo "<hr>";
-
+$alcenir = new Alcenir();
+$alcenir->showName();
 ?> 
