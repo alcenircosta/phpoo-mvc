111f59c47568aed4db79513ca996a3f6fc2c7083
Alcenir Costa <alcenir.1994@gmail.com>
2020-10-21 23:25:05 -0300

    Interfaces

--- index.php
+++ index.php
@@ -1,22 +1,26 @@
 <?php
- class Animal {
-   public function andar(){
-     echo "O animal andou <br/>";
-   }
-   public function correr(){
-     echo "O animal Correu";
-   }
- }
- class Cavalo extends Animal{
-  public function andar(){
-    echo "O cavalo andou <br/>";
-  }
-  public function andarRapido(){
-    $this->correr();
-  }
- }
- $animal = new Cavalo();
- $animal->andar();
- $animal->andarRapido();
  
+ interface Crud {
+   public function create();
+   public function read();
+   public function delete();
+   public function update();
+ }
+
+ class Noticias implements Crud {
+  public function create(){
+
+  }
+  public function read(){
+
+  }
+  public function delete(){
+
+  }
+  public function update(){
+    
+  }
+
+ }
+
  ?> 
