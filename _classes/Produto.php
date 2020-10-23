86d97d00345e2817c193c260ab52745960764b9b
Alcenir Costa <alcenir.1994@gmail.com>
2020-10-23 16:06:11 -0300

    Remove _models _classes folders and clean index.php

--- _classes/Produto.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace _classes;
-
-class Produto {
-    public function mostrarDetalhes(){
-        echo "Detalhes do produto da pasta _classes!";
-    }
-}
-?>
--- index.php
+++ index.php
@@ -1,21 +1,4 @@
 <?php
 
-class Pessoa {
-    private $nome;
-    private $idade;
-    public function __set($nome,$valor){
-        $this->nome = $valor;
-    }
-    public function __get($nome){
-        return $this->nome;
-    }
-
-
-}
-$pessoa = new Pessoa();
-$pessoa->nome="Alcenir";
-var_dump($pessoa);
-
-echo $pessoa->nome;
 
 ?> 
